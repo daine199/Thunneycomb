a946293a65bb8bc71c36d3573d639726feab4f83
daine199 <daine199@thunneycomb.com>
2016-01-20 01:58:36 +0800

    add form css class sample

--- wintercome/forms.py
+++ wintercome/forms.py
@@ -5,5 +5,5 @@ from django import forms
 
 
 class UserLogin(forms.Form):
-    userid = forms.CharField(label='User ID', max_length=32)
-    passwd = forms.CharField(label='Password', widget=forms.PasswordInput())
+    userid = forms.CharField(label='User ID', max_length=32, widget=forms.TextInput(attrs={"class":"form-control","placeholder":"UserID"}))
+    passwd = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={"class":"form-control","placeholder":"Password"}))
