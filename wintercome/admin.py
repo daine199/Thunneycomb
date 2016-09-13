3d6afb2cd88b294073bf67f1f2bc561df864be00
Daine.H <yifeng.huang@dianping.com>
2016-09-13 19:47:48 +0800

    add login model

--- wintercome/admin.py
+++ wintercome/admin.py
@@ -1,20 +1,20 @@
-from django.contrib import admin
-from .models import cmdTask,runLevel,winterUser
-# Register your models here.
-
-# class cmdTaskInline(admin.StackedInline):
-#     model = cmdTask
-#     extra = 3
-
-class runLevelAdmin(admin.ModelAdmin):
-    list_display = ('id','title','level')
-
-class winterUserAdmin(admin.ModelAdmin):
-    list_display = ('id','userid','runLevel','passwd')
-
-class cmdTaskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'ownerid','cmd','runlevel')
-
-admin.site.register(cmdTask,cmdTaskAdmin)
-admin.site.register(winterUser,winterUserAdmin)
-admin.site.register(runLevel,runLevelAdmin)
+# from django.contrib import admin
+# from .models import cmdTask,runLevel,winterUser
+# # Register your models here.
+#
+# # class cmdTaskInline(admin.StackedInline):
+# #     model = cmdTask
+# #     extra = 3
+#
+# class runLevelAdmin(admin.ModelAdmin):
+#     list_display = ('id','title','level')
+#
+# class winterUserAdmin(admin.ModelAdmin):
+#     list_display = ('id','userid','runLevel','passwd')
+#
+# class cmdTaskAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'title', 'ownerid','cmd','runlevel')
+#
+# admin.site.register(cmdTask,cmdTaskAdmin)
+# admin.site.register(winterUser,winterUserAdmin)
+# admin.site.register(runLevel,runLevelAdmin)
