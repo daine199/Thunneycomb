6ac0f56a501cab47f396a11859c3e3e9c3ee8648
Daine.H <yifeng.huang@dianping.com>
2016-09-14 11:03:57 +0800

    fix wintercome bug.

--- wintercome/urls.py
+++ wintercome/urls.py
@@ -1,13 +1,13 @@
-#
-# # Owner Daine.H
-# # Modify 2016-01-05
-#
-# from django.conf.urls import url
-# from . import views
-# from django.conf.urls.static import static
-#
-# urlpatterns = [
-#     url(r'^cmdindex/$', views.cmd_index_page, name='cmd_index_page'),
-#     url(r'^cmdindex_limit/$', views.cmd_index_page_limit, name='cmd_index_page_limit'),
-#     url(r'^basiccall/(?P<cmd_id>[0-9]+)/$', views.basic_cmd_run, name='basic-cmd-call')
-# ]
+
+# Owner Daine.H
+# Modify 2016-01-05
+
+from django.conf.urls import url
+from . import views
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^cmdindex/$', views.cmd_index_page, name='cmd_index_page'),
+    url(r'^cmdindex_limit/$', views.cmd_index_page_limit, name='cmd_index_page_limit'),
+    url(r'^basiccall/(?P<cmd_id>[0-9]+)/$', views.basic_cmd_run, name='basic-cmd-call')
+]
--- wintercome/views.py
+++ wintercome/views.py
@@ -1,46 +1,46 @@
-# from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+from .models import cmdTask, runLevel, winterUser
+import json, time
+
 #
-# # Create your views here.
-#
-# from .models import cmdTask, runLevel, winterUser
-# import json, time
-#
-# #
-# # def basic_cmd_run(request):
-# #     pass
-#
-# # def basic_cmd_run(request, cmd_id):
-# #     ret = cmdTask.objects.get(id=int(cmd_id)).baseCall()
-# #     res = json.dumps(ret)
-# #     return HttpResponse(res)
-# #     # return HttpResponse('Return code: {0} \n{1}'.format(resprint))
-# #     #return HttpResponse("You're looking at cmd {0}.".format(cmd_id))
-#
-# def basic_cmd_run(request,cmd_id):
-#     context = {}
-#     cmdtask = cmdTask.objects.get(id=int(cmd_id))
-#     res = cmdtask.baseCall()
-#     context['task'] = cmdtask
-#     context['code'] = res['returncode']
-#     context['result'] = res['returnprint']
-#     return render(request, 'wintercome/runresult.html', context)
-#
-#
-# def cmd_index_page_limit(request):
-#     # time.sleep(5000000)
-#     i = 0
-#     while i <= 999999:
-#         i += 1
-#         print("sleeping")
-#     context = {}
-#     cmdtasklist = cmdTask.objects.all()
-#     context['cmdtasklist'] = cmdtasklist
-#     print("sleep 500000")
-#     return render(request, 'wintercome/cmdlist.html', context)
-#
-# def cmd_index_page(request):
-#     context = {}
-#     cmdtasklist = cmdTask.objects.all()
-#     context['cmdtasklist'] = cmdtasklist
-#     return render(request, 'wintercome/cmdlist.html',context)
+# def basic_cmd_run(request):
+#     pass
+
+# def basic_cmd_run(request, cmd_id):
+#     ret = cmdTask.objects.get(id=int(cmd_id)).baseCall()
+#     res = json.dumps(ret)
+#     return HttpResponse(res)
+#     # return HttpResponse('Return code: {0} \n{1}'.format(resprint))
+#     #return HttpResponse("You're looking at cmd {0}.".format(cmd_id))
+
+def basic_cmd_run(request,cmd_id):
+    context = {}
+    cmdtask = cmdTask.objects.get(id=int(cmd_id))
+    res = cmdtask.baseCall()
+    context['task'] = cmdtask
+    context['code'] = res['returncode']
+    context['result'] = res['returnprint']
+    return render(request, 'wintercome/runresult.html', context)
+
+
+def cmd_index_page_limit(request):
+    # time.sleep(5000000)
+    i = 0
+    while i <= 999999:
+        i += 1
+        print("sleeping")
+    context = {}
+    cmdtasklist = cmdTask.objects.all()
+    context['cmdtasklist'] = cmdtasklist
+    print("sleep 500000")
+    return render(request, 'wintercome/cmdlist.html', context)
+
+def cmd_index_page(request):
+    context = {}
+    cmdtasklist = cmdTask.objects.all()
+    context['cmdtasklist'] = cmdtasklist
+    return render(request, 'wintercome/cmdlist.html',context)
