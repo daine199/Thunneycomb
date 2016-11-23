6328c9955d54c10a99f32db6f18fbde0e2789406
Daine.H <yifeng.huang@dianping.com>
2016-11-23 14:39:05 +0800

    add home entrance model and settings. modify error font size.

--- manage_dev.py
+++ manage_dev.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thunneycomb.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thunneycomb.dev")
 
     from django.core.management import execute_from_command_line
 
