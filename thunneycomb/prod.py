08e335087f3b4b55c87ca23f78a40aade1520a94
Daine.H <yifeng.huang@dianping.com>
2016-11-25 11:49:58 +0800

    fix rest settings

--- thunneycomb/prod.py
+++ thunneycomb/prod.py
@@ -171,8 +171,15 @@ ADMIN_SITE_HEADER = "Thunneycomb Admin"
 
 # REST
 LOGIN_ENABLE = True
-INSTALLED_APPS += ['rest_framework']
+INSTALLED_APPS += ['rest_framework',
+                   'rest_framework.authtoken'
+                   ]
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticatedOrReadOnly',),
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
+    )
 }
