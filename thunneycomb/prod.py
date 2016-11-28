e22e9374850a78814c8bca3f73fcbd00838f6005
Daine.H <yifeng.huang@dianping.com>
2016-11-28 21:23:51 +0800

    blog system base platycodon app online.

--- thunneycomb/prod.py
+++ thunneycomb/prod.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 from .appsetting.wiki_settings import *
 from .appsetting.rest_settings import *
+from .appsetting.platycodon_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
