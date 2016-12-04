fe79450dc9e601937c4ec5d435fe9a2e54c2ca49
Daine.H <yifeng.huang@dianping.com>
2016-12-04 22:34:55 +0800

    1.rm unsafe rest api

--- home/urls.py
+++ home/urls.py
@@ -9,11 +9,6 @@ urlpatterns = [
     url(r'^logout/$', views.logout_processor, name='logout_processor')
 ]
 
-urlpatterns += [
-    url(r'^entrance/$', views.entrance_list),
-    url(r'^entrance/(?P<pk>[0-9]+)/$', views.entrance_detail),
-]
-
 router = routers.DefaultRouter()
 router.register('HomeEntrance', views.EntranceViewSet)
 router.register('PlatycodonEntrance', PlatycodonViewSet)
