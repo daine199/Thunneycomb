f8f1f35618fc5dd7435843b76c858010d7b6d90d
Daine.H <yifeng.huang@dianping.com>
2017-01-03 23:55:57 +0800

    1.update env settings.

--- thunneycomb/env_check.py
+++ thunneycomb/env_check.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 # Daine.H
 
+import os
 import linecache
 
 
 #  取环境设定。返回默认值dev或生产环境 product
 def get_env(env='dev'):
     try:
-        return linecache.getline('~/webapps/appenv', 1).lower().strip().split('=')[-1] or env
+        user_path = os.getenv('HOME')
+        appenv = os.path.join(user_path, 'webapp/appenv')
+        return linecache.getline(appenv, 1).lower().strip().split('=')[-1] or env
     except IOError:
         return env
