c86ee103d8aa93f4952344679ec2bde0690af83b
Daine.H <yifeng.huang@dianping.com>
2017-01-04 12:08:11 +0800

    1.update Entrance permissions.

--- home/admin.py
+++ home/admin.py
@@ -8,9 +8,4 @@ class EntranceAdmin(admin.ModelAdmin):
     list_display = ('id', 'entrance', 'entrance_url')
 
 
-class EntrancePermissionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'has_index_perm')
-
-
 admin.site.register(Entrance, EntranceAdmin)
-admin.site.register(EntrancePermission, EntrancePermissionAdmin)
