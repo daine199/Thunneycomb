96ffc8ccb6f49b29b676f53a93570836ab95d7f2
Daine.H <yifeng.huang@dianping.com>
2017-04-28 15:41:26 +0700

    fix package router

--- pet_api/models.py
+++ pet_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class PetAPI(models.Model):
+    user = models.CharField(max_length=32)
+    description = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return self.user
--- pet_api/views.py
+++ pet_api/views.py
@@ -8,6 +8,14 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def apt_test(request):
-    print(request.method)
-    content = {'this': 'Pet API Tests', 'method': request.method}
+    request_method = request.method
+    content = {'this': 'Pet API Tests', 'method': request_method}
+    if 'POST' == request_method:
+        print("In Create")
+    if 'GET' == request_method:
+        print("In Retrieve")
+    if 'PUT' == request_method or 'PATCH' == request_method:
+        print("In PUT/PATCH")
+    if 'DELETE' == request_method:
+        print("In DELETE")
     return HttpResponse(json.dumps(content), content_type='application/javascript;charset=utf-8')
