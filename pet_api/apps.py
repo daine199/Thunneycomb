9cad06c5225650dbe95862760e4d9d8fe9ac413d
Daine.H <yifeng.huang@dianping.com>
2017-04-28 13:43:55 +0700

    1.fix deploy perms issue.

---
+++ pet_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PetApiConfig(AppConfig):
+    name = 'pet_api'
---
+++ pet_api/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import json
+
+# Create your views here.
+
+
+def pet_get(request):
+    if request.method == 'GET':
+        content = {'this': 'Pet API GET Tests', 'method': request.method}
+    return HttpResponse(json.dumps(content))
