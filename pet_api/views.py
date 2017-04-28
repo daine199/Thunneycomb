d66b4a65f1edddcd3130ac9a0b1819fdac6a6ca8
Daine.H <yifeng.huang@dianping.com>
2017-04-28 14:51:16 +0700

    fix package router

--- pet_api/views.py
+++ pet_api/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import json
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 
-def pet_get(request):
-    if request.method == 'GET':
-        content = {'this': 'Pet API GET Tests', 'method': request.method}
+@csrf_exempt
+def apt_test(request):
+    print(request.method)
+    content = {'this': 'Pet API Tests', 'method': request.method}
     return HttpResponse(json.dumps(content), content_type='application/javascript;charset=utf-8')
