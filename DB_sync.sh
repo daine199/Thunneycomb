d3ff34ced7a12d5d7c6c3ee9d390fcf6528b18c2
yifeng.huang <yifeng.huang@dianping.com>
2018-12-11 21:22:58 +0800

    title:README文档更新
    更新了部署可能遇到的坑。

--- DB_sync.sh
+++ DB_sync.sh
@@ -5,9 +5,11 @@ python manage.py makemigrations home
 python manage.py makemigrations pet_api
 python manage.py makemigrations thunder_token
 python manage.py makemigrations platycodon
+python manage.py makemigrations basis_components
 
 python manage.py migrate
 python manage.py migrate home
 python manage.py migrate pet_api
 python manage.py migrate thunder_token
 python manage.py migrate platycodon
+python manage.py migrate basis_components
---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM daine199/django-uwsgi
+ENV PYTHONUNBUFFERED 1
+RUN mkdir /code
+WORKDIR /code
+ADD requirements.txt /code/
+RUN pip install -r requirements.txt
+ADD . /code/
+
+
+
