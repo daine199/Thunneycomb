16440cd0a31d812ca60b4dc07b8c1f25a2c4127a
daine199 <daine199@thunneycomb.com>
2019-09-06 17:54:22 +0800

    2019-09-05 New Thunneycomb ， 重写完成 1. home 可用 2. platycodon 可用 3. wiki 可用 4. 删除django-rest

--- platycodon/urls.py
+++ platycodon/urls.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 from django.urls import path, include
 
+from platycodon import views
 from platycodon.admin import admin_site
 
 
 urlpatterns = [
     path('platycodon-admin', admin_site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+
+    path("id/", views.get_platycodon_by_id, name='Platycodon Detail')
 ]
 
 
--- platycodon/views.py
+++ platycodon/views.py
@@ -1,6 +1,32 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+import json
+import logging
 
+from django.core.serializers.json import DjangoJSONEncoder
 from django.shortcuts import render
+from django.core.serializers import serialize
+from django.http import JsonResponse, HttpResponse
+
+from platycodon.models import Platycodon
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
 
 # Create your views here.
+
+def get_platycodon_by_id(request):
+    platycodon_id = 0
+    if request.method == 'GET':
+        platycodon_id = request.GET.get('id')
+        logger.info(platycodon_id)
+    platycodon = Platycodon.objects.filter(id=platycodon_id)
+
+    # print(serialize('json', platycodon))
+
+    # class JsonResponse(data, encoder=DjangoJSONEncoder, safe=True, json_dumps_params=None, **kwargs)
+    return JsonResponse(serialize("json", platycodon), safe=False)
+
+
+
