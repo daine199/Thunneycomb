81706b19dd1b02472dd9b7f4c1961a20c83a715c
黄一峰 <yifeng.huang@dianping.com>
2019-09-26 14:54:21 +0800

    2019-09-26 修复新版本Django登录判断异常导致无法登录问题。

--- home/urls.py
+++ home/urls.py
@@ -18,11 +18,13 @@ urlpatterns += [
 
 urlpatterns += [
     path('index/', views.index),
+    path('accounts/login/', views.login_user, name='login_page'),
+    path('logout/', views.logout_processor, name='logout_processor')
 ]
 
 #  Deploy ROUTER
 urlpatterns += [
-    path(r'deploy/', views.deploy_entrance, name='deploy_entrance')
+    path('deploy/', views.deploy_entrance, name='deploy_entrance')
 ]
 
 
